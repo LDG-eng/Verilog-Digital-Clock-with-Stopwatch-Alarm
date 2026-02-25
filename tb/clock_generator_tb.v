9dac26b8b2bb6b419fb25b50279df723213130d2
LDG-eng <rak2love69@gmail.com>
2026-02-25 12:26:09 +0900

    Update clock_generator_tb.v

--- tb/clock_generator_tb.v
+++ tb/clock_generator_tb.v
@@ -1,1 +1,19 @@
+`timescale 10us/1us		
 
+module clock_generator_tb ();
+  reg Clock_5k, reset;
+  
+  clock_gen_module CGM(Clock_5k, reset, Clock_1Sec, Clock_1mSec);
+  
+  initial
+  begin
+  
+    Clock_5k = 1; reset = 1; 		//reset
+    #50 reset = 0;
+    #50 reset = 1;	
+
+  end
+
+  always #10 Clock_5k = ~Clock_5k;	//make 5kHz
+
+endmodule
